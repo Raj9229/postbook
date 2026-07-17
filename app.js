287f1400affb95722363df4cd292dce90165731e
Raj Gupta <krishnagupta9229@gmail.com>
2026-07-17 15:35:34 +0530

    multer

--- app.js
+++ app.js
@@ -9,11 +9,36 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
+const path = require('path');
+const multer = require('multer');
+const crypto = require('crypto');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images/uploads/');
+    },
+    filename: function (req, file, cb) {
+        crypto.randomBytes(12, (err, bytes) => {
+        const fn = bytes.toString('hex') + path.extname(file.originalname);
+        cb(null, fn);
+    })
+}
+});
 
+const upload = multer({ storage: storage });
 
 app.get("/",(req,res)=>{
     res.render("index");
+});
+
+//test multer
+app.get("/test",(req,res)=>{
+    res.render("test");
+});
+
+app.post("/test",upload.single('image'), (req,res)=>{
+    console.log(req.file);
+    res.send("File uploaded successfully");
 });
 
 
@@ -48,10 +73,10 @@ app.get("/login", (req,res)=>{
 
 app.post("/login", async (req,res)=>{
     const { email, password } = req.body;
-//check if the user exists in the database
+    //check if the user exists in the database
     let user = await userModel.findOne({email});
     if (!user) return res.status(400).send("something went wrong");
-//compare the password provided by the user with the hashed password stored in the database
+    //compare the password provided by the user with the hashed password stored in the database
     bcrypt.compare(password, user.password ,(err, result)=>{
         if (result) {
             const token = jwt.sign({ email: user.email, userid: user._id }, "secretkeyyy");
